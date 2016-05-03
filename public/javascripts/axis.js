fdac7e05da8166883ecd28285a533b192e2aa8c4
Simon Diemert <simon.diemert@gmail.com>
2016-05-03 11:40:02 -0700

    marks on axis appear

--- public/javascripts/axis.js
+++ public/javascripts/axis.js
@@ -65,9 +65,13 @@ function makeText(id, x, y, text, size, rotate){
     return e; 
 }
 
+function makeGroup(){
+   return document.createElementNS("http://www.w3.org/2000/svg","g");
+}
+
 function makeDayAxis(id, start){
     
-    var g = $(document.createElementNS("http://www.w3.org/2000/svg","g"));
+    var g = $(makeGroup());
     g.append(makeLine(id, 20, start, 1000 - 20, start, "black", 2));
 
     var tickX = 0;
@@ -80,7 +84,7 @@ function makeDayAxis(id, start){
         tickX = 20+(i*40);
         tickId = id+'-tick'+i;
         
-        innerGroup = $(document.createElementNS("http://www.w3.org/2000/svg","g"));
+        innerGroup = $(makeGroup());
         innerGroup.append(makeLine(tickId, tickX, start-5, tickX, start+5, "black", 2));
         innerGroup.append(makeText(tickId + "-label", tickX-15, start+20, ""+i+":00", 14, 45));
         
@@ -92,20 +96,78 @@ function makeDayAxis(id, start){
 }
 
 /**
+ * Returns the position of the date on a scale defined by rangeMin and rangeMax
+ * @param t {Date}
+ * @param rangeMin {number}
+ * @param rangeMax {number}
+ */
+function scaleDayToAxis(t, rangeMin, rangeMax){
+
+    // compute number of minutes in the day.
+    var mins = t.getHours() * 60 + t.getMinutes();
+    return Math.floor((mins/1440) * (rangeMax - rangeMin) + rangeMin);
+
+}
+
+/**
+ * 
+ * @param id {string}
+ * @param x {number}
+ * @param y {number}
+ * @param r {number}
+ * @param color {string}
+ */
+function makeCircle(id, x, y, r, color){
+    var e = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    e.setAttribute("cx", x);
+    e.setAttribute("cy", y);
+    e.setAttribute("r", r);
+    e.setAttribute("id", id);
+    e.setAttribute("style", "fill:"+color+";");
+    
+    return e; 
+
+}
+
+/**
+ * 
+ * @param hOffset {number}
+ * @param vOffset {number}
+ * @param event {object}
+ * @param event.datetime {Date}
+ */
+function makeEventMark(hOffset, vOffset, event) {
+
+    var x = scaleDayToAxis(event.datetime, hOffset, 980);
+
+    return makeCircle(null, x, vOffset, 5, 'blue');
+
+}
+/**
  * 
  * @param canvas {object}
  * @param events {array}
  */
 function drawSingleTimeline(canvas, events){
 
-
-    $(canvas).append(makeDayAxis('timeline-1', 100));
+    var timeLineGroup = $(makeGroup());
+    
+    timeLineGroup.append(makeDayAxis('timeline-1', 100));
+    
+    
+    for(var e = 0; e < events.length; e++){
+        timeLineGroup.append(makeEventMark(20, 100, events[e]));
+    }
+    
+    $(canvas).append(timeLineGroup);
+    
+     
     
 }
 
-function drawTimelines(canvas){
+function drawTimelines(canvas, data){
 
-   drawSingleTimeline(canvas);
+   drawSingleTimeline(canvas, data.slice(1, 6));
 
 }
 
@@ -124,7 +186,7 @@ function showViz(err, data) {
 
     wrapper.append(canvas);
     
-    drawTimelines(canvas);
+    drawTimelines(canvas, data);
 
 }
 

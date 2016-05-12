ae02079dc6542638fedebfcec80ba9f884b7c86e
Simon Diemert <simon.diemert@gmail.com>
2016-05-12 13:47:01 -0700

    displays sequences one after each other.

--- routes/study.js
+++ routes/study.js
@@ -26,21 +26,52 @@ router.get('/', function(req, res, next) {
 
 router.get('/data', function(req, res, next){
 
-    
-    d.getSequence(0, function(result){
-
-        var toSend = []; 
-        
-        for(var i = 0; i < result.length; i++){
-            toSend.push(result[i].toTransferObject()); 
-        }
-        
-        res.json(toSend);
-        
-    });
+    return sendSequence(0, res);
 
 });
 
+/**
+ *  Handles adherence responses (yes/no). 
+ * 
+ *  Returns a response with the next data.
+ */
+router.post("/response", function(req, res){
+
+    if(!req.query || !req.query.adherent || !req.query.sequence){
+        res.status(400).send("Bad Request"); 
+    }else{
+
+        console.log("RESPONSE " + JSON.stringify(req.query));
+        
+        // get user from cookie
+        
+        // send result to db.
+
+        // send next sequence
+        sendSequence(req.query.sequence+1, res);
+    }
+    
+});
+
+
+function sendSequence(seq, res){
+    d.getSequence(seq, function(result){
+
+        if(result.length === 0){
+
+            res.status(204).send("DONE");
+
+        }else{
+            var toSend = [];
+            for(var i = 0; i < result.length; i++){
+                toSend.push(result[i].toTransferObject());
+            }
+            res.json({sequenceNo : seq, data : toSend});
+        }
+
+
+    }); 
+}
 
 
 module.exports = router;
